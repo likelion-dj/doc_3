31600cdc2ce8c9bcc9ebe57a6d64295d3795e722
jhs512 <jangka512@gmail.com>
2022-08-22 20:57:57 +0900

    세팅

---
+++ src/main/java/com/ll/exam/doc_3/Doc3Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.doc_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Doc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/doc_3/Doc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.doc_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
